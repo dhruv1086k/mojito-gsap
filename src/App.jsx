0b9ef6cfe72bf09b455fab2f74ceea1c79d748f4
Dhruv <dhruvpal.0905@gmail.com>
2025-06-25 19:24:10 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText); // gsap doesn't automatically activates plugin so we use this line to activate the plugins
 
 const App = () => {
-  return <div className="">App component</div>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 };
 
 export default App;
